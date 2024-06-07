bbf43be8ce07807f05dc44f2fdc08dbc1a99b2fa
Nwakanma Dominion <118551746+Immadominion@users.noreply.github.com>
2024-06-07 15:52:58 +0100

    FIX/ time not reactive

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,19 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Falz the bahd guy',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
